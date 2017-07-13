2f5b1ef35f4e25bd8cfd9f122c558c1f8f025830
Vincent Francolin <vfrancolin@tessarine.com>
2017-07-13 17:12:43 +0200

    add index

---
+++ server/src/routes/index.ts
@@ -0,0 +1,8 @@
+import express = require("express");
+import {router as reservationRouter} from "./reservation"
+import {router as roomRouter} from "./room";
+
+export let router = express.Router();
+
+router.use("/room",roomRouter);
+router.use("/reservation",reservationRouter);
